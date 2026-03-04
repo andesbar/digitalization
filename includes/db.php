a67157b3e8504b122a5c270dbf361b0d8a07f40a
Mantra Digital Indonesia <inbox.agusmunandar@gmail.com>
2026-03-05 00:37:20 +0700

    Update

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 // D:\MEGAPROJECT\includes\db.php
 
-$host = "db-mega-project"; // <--- SESUAIKAN DENGAN YAML DI ATAS
+$host = "db-digitalization"; // <--- SESUAIKAN DENGAN YAML DI ATAS
 $db   = "mega_db";
 $user = "mega_admin";
 $pass = "Megadmin";
